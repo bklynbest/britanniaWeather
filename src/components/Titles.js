5d3815c197ea1b1a717383525dca1213a2fe30ae
Jonathan Momperousse <bklynbest718@gmail.com>
2018-09-18 17:45:15 +0000

    finished app

---
+++ src/components/Titles.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Titles = () => (
+	<div>
+		<h1 className="title-container__title">Weather Finder</h1>
+		<h3 className="title-container__subtitle">Find out temperature, conditions and more...</h3>
+		<center>Britannia Dev 2018</center>
+	</div>
+);
+
+export default Titles;
